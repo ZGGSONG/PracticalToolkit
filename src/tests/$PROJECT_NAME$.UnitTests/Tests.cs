741437259817fc759ead45fbfcbd4ea63c7c50cc
zggsong <49741009+ZGGSONG@users.noreply.github.com>
2024-07-05 13:30:59 +0800

    Initial commit

---
+++ src/tests/$PROJECT_NAME$.UnitTests/Tests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace $PROJECT_NAME$.UnitTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public async Task AsyncTest()
+        {
+            using var cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+            var cancellationToken = cancellationTokenSource.Token;
+
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+        }
+		
+        [TestMethod]
+        public void Test()
+        {
+			"".Should().Be("");
+        }
+    }
+}
