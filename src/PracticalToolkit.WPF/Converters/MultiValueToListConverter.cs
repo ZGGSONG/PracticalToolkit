09474d283f10abe6e5641e6c409b8a2482f262e6
zggsong <zggsong@foxmail.com>
2024-07-06 15:57:42 +0800

    chore

---
+++ src/PracticalToolkit.WPF/Converters/MultiValueToListConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PracticalToolkit.WPF.Converters;
+
+public sealed class MultiValueToListConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        return values.ToList();
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return Enumerable.Repeat(Binding.DoNothing, targetTypes.Length).ToArray();
+    }
+}
