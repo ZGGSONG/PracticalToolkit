7c39d4c1f630e495d8ff2339221a1c070befdd00
zggsong <zggsong@foxmail.com>
2024-07-15 09:21:02 +0800

    feat: Add Icon Size to XamlIcon to quickly adjust font size

--- src/PracticalToolkit.WPF/Controls/XamlIcon.cs
+++ src/PracticalToolkit.WPF/Controls/XamlIcon.cs
@@ -12,7 +12,12 @@ public class XamlIcon : Label
             new FrameworkPropertyMetadata(Icons.None, FrameworkPropertyMetadataOptions.AffectsArrange,
                 IconChangedCallback));
 
-    private readonly Dictionary<string, Viewbox> _globalIcon = [];
+    public static readonly DependencyProperty IconSizeProperty =
+        DependencyProperty.Register(nameof(IconSize), typeof(double), typeof(XamlIcon),
+            new FrameworkPropertyMetadata(12.0, FrameworkPropertyMetadataOptions.AffectsMeasure,
+                IconSizeChangedCallback));
+
+    private readonly Dictionary<string, Viewbox> _globalIcon = new();
 
     public XamlIcon()
     {
@@ -28,6 +33,12 @@ public class XamlIcon : Label
         set => SetValue(IconProperty, value);
     }
 
+    public double IconSize
+    {
+        get => (double)GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
+    }
+
     public override void OnApplyTemplate()
     {
         base.OnApplyTemplate();
@@ -35,6 +46,11 @@ public class XamlIcon : Label
     }
 
     private static void IconChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        (d as XamlIcon)?.RefreshControls();
+    }
+
+    private static void IconSizeChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         (d as XamlIcon)?.RefreshControls();
     }
@@ -51,6 +67,10 @@ public class XamlIcon : Label
         var key = $"/PracticalToolkit.WPF;component/Resources/Icons/{iconStr}.xaml";
         if (_globalIcon.TryGetValue(key, out var value))
         {
+            value.Width = IconSize;
+            value.Height = IconSize;
+            value.HorizontalAlignment = HorizontalAlignment.Stretch;
+            value.VerticalAlignment = VerticalAlignment.Stretch;
             Content = value;
         }
         else
@@ -58,6 +78,10 @@ public class XamlIcon : Label
             var info = Application.GetResourceStream(new Uri(key, UriKind.Relative)) ?? throw new NullReferenceException(nameof(Icon));
             using var stream = info.Stream;
             var page = (Viewbox)XamlReader.Load(info.Stream);
+            page.Width = IconSize;
+            page.Height = IconSize;
+            page.HorizontalAlignment = HorizontalAlignment.Stretch;
+            page.VerticalAlignment = VerticalAlignment.Stretch;
             Content = page;
             _globalIcon.Add(key, page);
         }
